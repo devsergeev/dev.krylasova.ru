b2790e330a71e45077a5c1277a5823c149a16867
devsergeev <sergeev.vasily.v@gmail.com>
2021-04-10 12:43:41 +0000

    Добавил user email change confirm

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -10,6 +10,7 @@ interface UserRepository
     public function hasByNetwork(NetworkIdentity $identity): bool;
     public function findByJoinConfirmToken(string $token): ?User;
     public function findByPasswordResetToken(string $token): ?User;
+    public function findByNewEmailToken(string $token): ?User;
     /**
      * @throws \DomainException
      */
