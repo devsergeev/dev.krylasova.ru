903072d7e7e70b24496100f9fa13c9bb7a592837
devsergeev <sergeev.vasily.v@gmail.com>
2021-04-10 12:06:51 +0000

    Added password reset

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -9,6 +9,7 @@ interface UserRepository
     public function hasByEmail(Email $email): bool;
     public function hasByNetwork(NetworkIdentity $identity): bool;
     public function findByJoinConfirmToken(string $token): ?User;
+    public function findByPasswordResetToken(string $token): ?User;
     /**
      * @throws \DomainException
      */
