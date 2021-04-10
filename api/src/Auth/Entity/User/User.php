622f8477c232e5aa6f64fc6a0946dabe2c4b4a73
devsergeev <sergeev.vasily.v@gmail.com>
2021-04-10 12:29:40 +0000

    Добавил user email change request

--- api/src/Auth/Entity/User/User.php
+++ api/src/Auth/Entity/User/User.php
@@ -19,6 +19,8 @@ class User
     private ?Token $joinConfirmToken = null;
     private ArrayObject $networks;
     private ?Token $passwordResetToken = null;
+    private ?Email $newEmail = null;
+    private ?Token $newEmailToken = null;
 
     private function __construct(Id $id, DateTimeImmutable $date, Email $email, Status $status)
     {
@@ -74,15 +76,6 @@ class User
         return $user;
     }
 
-    /**
-     * @return NetworkIdentity[]
-     */
-    public function getNetworks(): array
-    {
-        /** @var NetworkIdentity[] */
-        return $this->networks->getArrayCopy();
-    }
-
     public function requestPasswordReset(Token $token, DateTimeImmutable $date): void
     {
         if (!$this->isActive()) {
@@ -113,6 +106,21 @@ class User
             throw new DomainException('Incorrect current password.');
         }
         $this->passwordHash = $hasher->hash($new);
+    }
+
+    public function requestEmailChanging(Token $token, DateTimeImmutable $date, Email $email): void
+    {
+        if (!$this->isActive()) {
+            throw new DomainException('User is not active.');
+        }
+        if ($this->email->isEqualTo($email)) {
+            throw new DomainException('Email is already same.');
+        }
+        if ($this->newEmailToken !== null && !$this->newEmailToken->isExpiredTo($date)) {
+            throw new DomainException('Changing is already requested.');
+        }
+        $this->newEmail = $email;
+        $this->newEmailToken = $token;
     }
 
     public function isWait(): bool
@@ -153,5 +161,24 @@ class User
     public function getPasswordResetToken(): ?Token
     {
         return $this->passwordResetToken;
+    }
+
+    public function getNewEmail(): ?Email
+    {
+        return $this->newEmail;
+    }
+
+    public function getNewEmailToken(): ?Token
+    {
+        return $this->newEmailToken;
+    }
+
+    /**
+     * @return NetworkIdentity[]
+     */
+    public function getNetworks(): array
+    {
+        /** @var NetworkIdentity[] */
+        return $this->networks->getArrayCopy();
     }
 }
