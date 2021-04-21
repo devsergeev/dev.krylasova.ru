927c4a0a8cf2e54fe654687352b504d7c3ef581f
devsergeev <sergeev.vasily.v@gmail.com>
2021-04-21 18:04:31 +0000

    Добавил консольную команду для загрузки фикстур

--- api/config/dev/console.php
+++ api/config/dev/console.php
@@ -2,17 +2,40 @@
 
 declare(strict_types=1);
 
-use Doctrine\ORM\Tools\Console\Command\SchemaTool;
+use App\Console\FixturesLoadCommand;
 use Doctrine\Migrations\Tools\Console\Command;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand;
+use Psr\Container\ContainerInterface;
 
 return [
+    FixturesLoadCommand::class => static function (ContainerInterface $container) {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{fixture_paths:string[]} $config
+         */
+        $config = $container->get('config')['console'];
+
+        $em = $container->get(EntityManagerInterface::class);
+
+        return new FixturesLoadCommand(
+            $em,
+            $config['fixture_paths'],
+        );
+    },
+
     'config' => [
         'console' => [
             'commands' => [
-                SchemaTool\DropCommand::class,
+                FixturesLoadCommand::class,
+
+                DropCommand::class,
 
                 Command\DiffCommand::class,
                 Command\GenerateCommand::class,
+            ],
+            'fixture_paths' => [
+                __DIR__ . '/../../src/Auth/Fixture',
             ],
         ],
     ],
