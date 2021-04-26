0b3c24f3f6ca74cb6deff8fb8e533352f930752f
devsergeev <sergeev.vasily.v@gmail.com>
2021-04-26 18:12:12 +0000

    Migrated to Twig

--- api/src/Auth/Service/JoinConfirmationSender.php
+++ api/src/Auth/Service/JoinConfirmationSender.php
@@ -10,28 +10,31 @@ use App\Frontend\FrontendUrlGenerator;
 use RuntimeException;
 use Swift_Mailer;
 use Swift_Message;
+use Twig\Environment;
 
 class JoinConfirmationSender
 {
     private Swift_Mailer $mailer;
     private FrontendUrlGenerator $frontend;
+    private Environment $twig;
 
-    public function __construct(Swift_Mailer $mailer, FrontendUrlGenerator $frontend)
+    public function __construct(Swift_Mailer $mailer, FrontendUrlGenerator $frontend, Environment $twig)
     {
         $this->mailer = $mailer;
         $this->frontend = $frontend;
+        $this->twig = $twig;
     }
 
     public function send(Email $email, Token $token): void
     {
-        $message = (new Swift_Message('Join Confirmation'))
+        $message = (new Swift_Message('Подтверждение регистрации'))
             ->setTo($email->getValue())
-            ->setBody($this->frontend->generate('join/confirm', [
-                'token' => $token->getValue(),
-            ]));
+            ->setBody($this->twig->render('auth/join/confirm.html.twig', [
+                'url' => $this->frontend->generate('join/confirm', ['token' => $token->getValue()]),
+            ]), 'text/html');
 
         if ($this->mailer->send($message) === 0) {
-            throw new RuntimeException('Unable to send email.');
+            throw new RuntimeException('Ошибка отправки электронного письма.');
         }
     }
 }
