1e3433182e7a8694da4113f7ca076bed84d33061
devsergeev <sergeev.vasily.v@gmail.com>
2021-11-11 23:03:34 +0000

    Webpack configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,20 @@
 import './style.css'
 import printMe from './print.js';
 
+if (process.env.NODE_ENV === 'development') {
+  console.warn('Is dev mode');
+}
+
 const component = (itemText) => {
   const element = document.createElement('div')
   element.innerHTML = itemText
   element.classList.add('item')
   element.classList.add('icon')
-  element.onclick = printMe(() => createNewElement(itemText))
+  element.onclick = printMe(() => {
+    const element = document.createElement('div')
+    element.innerHTML = itemText
+    document.body.appendChild(element)
+  })
   return element
 }
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,69 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, options) => {
   const isProd = options.mode === 'production'
   const isDev = !isProd
 
+  const plugins = () => {
+    const plugins = [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new HtmlWebpackPlugin({
+        title: options.mode,
+        template: path.resolve(__dirname, './src/index.html.twig'),
+        filename: path.resolve(__dirname, '../backend/templates/index.html.twig'),
+        favicon: './src/favicon.svg',
+        alwaysWriteToDisk: true
+      }),
+      new HtmlWebpackHarddiskPlugin(),
+    ]
+
+    if (isDev) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+        chunkFilename: "[id].[contenthash].css",
+      }));
+    }
+
+    return plugins
+  }
+
+  const cssLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : "style-loader",
+      'css-loader'
+    ]
+  }
+
   return {
     //target: isDev ? 'web' : ['web', 'es5'],
     entry: {
       index: './src/index.js',
-      print: './src/print.js',
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, '../backend/public/assets'),
       publicPath: '/assets/',
       clean: true,
     },
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+    },
+    // devtool: isDev ? 'inline-source-map' : false,
     devServer: {
       client: {
         overlay: true,
@@ -28,33 +74,25 @@ module.exports = (env, options) => {
         publicPath: '/assets/',
       },
       port: 3000,
-      hot: false,
-      liveReload: true,
+      hot: true,
+      liveReload: false,
       proxy: {
         path: '**',
         target: 'http://dev.krylasova.local',
         secure: false,
       },
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: options.mode,
-        template: path.resolve(__dirname, './src/index.html.twig'),
-        filename: path.resolve(__dirname, '../backend/templates/index.html.twig'),
-        favicon: './src/favicon.svg',
-        alwaysWriteToDisk: true
-      }),
-      new HtmlWebpackHarddiskPlugin()
-    ],
-    devtool: isDev ? 'inline-source-map' : false,
+    plugins: plugins(),
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          include: path.resolve(__dirname, 'src'),
+          use: cssLoaders(),
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          include: path.resolve(__dirname, 'src'),
           type: 'asset/resource',
         },
       ],
